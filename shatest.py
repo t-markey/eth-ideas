4d07902892c9d37f98c05773b779169090a83171
tom_m <t.markey@mailfence.com>
2021-01-22 00:18:13 -0400

    more mining

--- shatest.py
+++ shatest.py
@@ -1,4 +1,7 @@
 from hashlib import sha256
+import time
+
+
 
 #this is 64 bit hexadecimal 
 #print( SHA256("ABC"))
@@ -13,11 +16,12 @@ def mine(block_number, transactions, previous_hash, prefix_zeros):
 		text = str(block_number) + transactions + previous_hash + str(nonce)
 		new_hash = SHA256(text)
 		if new_hash.startswith(difficulty * '0'):
-			print('______you receive bitcoins_______')
-			print( new_hash)
-			return
+			print(f'______You mined bitcoins with nonce value: {nonce}')
+			return new_hash
+			
 		print(new_hash)
-	return new_hash
+	 
+	raise BaseException(f'Couldn\'t find hash after {nonce} trys')
 
 
 
@@ -27,9 +31,16 @@ if __name__ == '__main__':
 	Joe -> Molly -> 30,
 	Growgoo -> Tom -> 20
 	'''
-
 	#number of prevailing 0's
 	difficulty =4
+
+
+	start = time.time()
+	print("Starting Mining")
 	new_hash = mine(5,transactions,'958a96ccd2e2dc1d871449661c53eede51b84c6de14148dd1922612a75b103a3',difficulty )
-	
+	total_time = str((time.time() - start))
+	print(f"Ending Mining which took : {total_time} seconds.")
+
+
+
 	print(new_hash)
